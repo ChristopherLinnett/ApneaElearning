e8b38481a5286ad1c34fdc4f96aa9137acac4181
Christopher Linnett <chrislinnett45389@gmail.com>
2022-05-04 17:17:15 +1000

    add routing to module page start layout for module page

--- src/app/content/overview/overview.page.ts
+++ src/app/content/overview/overview.page.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overview.page.scss'],
 })
 export class OverviewPage implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
--- src/app/course-landing/course-landing.page.ts
+++ src/app/course-landing/course-landing.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserService } from '../users/user.service';
 import { CourseService } from './course.service';
 
@@ -9,10 +10,13 @@ import { CourseService } from './course.service';
 })
 export class CourseLandingPage implements OnInit {
   thisCourse: {title: String, description: String};
-  constructor(private courseService: CourseService, private userService: UserService) { }
+  constructor(private courseService: CourseService, private userService: UserService, private router: Router) { }
 
 ngOnInit() {
   this.thisCourse = this.courseService.getCourse()
+}
+onClickModule() {
+this.router.navigate(['overview'])
 }
 
 
