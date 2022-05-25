9b27ec6a0540260babcfa3a49a4f882fd77c2632
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-05-26 01:38:42 +1000

    made changes to student and course data structure

--- src/app/instructor/createcourse/studentconstructor.class.ts
+++ src/app/instructor/createcourse/studentconstructor.class.ts
@@ -1,11 +1,30 @@
 export class StudentConstructor {
   //basic datastructure for a student
   firstName: string;
+  role: string;
   lastName: string;
   email: string;
-  constructor(firstName: string, lastName: string,  email: string, ) {
+  availableCourses: any[]
+  password: string
+  constructor(firstName: string, lastName: string,  email: string, role = "student") {
     this.firstName = firstName;
     this.lastName = lastName;
+    this.role = role
     this.email = email;
+    this.availableCourses = []
+    this.password = `${firstName.toLowerCase()}${lastName.toLowerCase()}`
+  }
+}
+
+export class CourseConstructor {
+  courseDate: string
+  courseType: string
+  courseInstructor: string
+  students: string[]
+  constructor(courseDate: string, courseType: string, courseInstructor: string, students: string[]=null){
+  this.courseDate = courseDate
+  this.courseType = courseType
+  this.courseInstructor = courseInstructor
+  this.students = students
   }
 }
