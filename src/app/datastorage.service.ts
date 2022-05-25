bf160802431fbf2eafd4e406bf3c77191fb37452
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-05-25 21:21:25 +1000

    initialise storage

--- src/app/datastorage.service.ts
+++ src/app/datastorage.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DatastorageService {
+  constructor(private storage: Storage) {
+    this.init();
+  }
 
-  constructor() { }
+  async init() {
+    const storage = await this.storage.create();
+  }
 }
