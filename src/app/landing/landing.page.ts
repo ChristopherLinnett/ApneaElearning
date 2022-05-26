99b45da3500e495a42e9c2363c7b9c6627002801
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-05-26 16:01:13 +1000

    add actionsheet for option button

--- src/app/landing/landing.page.ts
+++ src/app/landing/landing.page.ts
@@ -1,25 +1,17 @@
 import { ViewChild } from '@angular/core';
-import {
-  Component,
-  Injectable,
-  OnChanges,
-  OnInit,
-  SimpleChanges,
-} from '@angular/core';
-import Swiper, { SwiperOptions } from 'swiper';
+import { Component, OnInit } from '@angular/core';
+import { SwiperOptions } from 'swiper';
 import { SwiperComponent } from 'swiper/angular';
 import SwiperCore, {EffectCoverflow} from "swiper";
 SwiperCore.use([EffectCoverflow])
 import { Router } from '@angular/router';
-import { ModalController } from '@ionic/angular';
+import { ActionSheetController, ModalController, PopoverController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 import { CourseService } from '../course-landing/course.service';
 import { CreatecourseComponent } from '../instructor/createcourse/createcourse.component';
 import { UserService } from '../users/user.service';
 import { AfterContentChecked } from '@angular/core';
-import { CourseConstructor } from '../instructor/createcourse/studentconstructor.class';
 import { OnDestroy } from '@angular/core';
-import { HostListener } from '@angular/core';
 import { DatastorageService } from '../datastorage.service';
 @Component({
   selector: 'app-landing',
@@ -43,6 +35,7 @@ export class LandingPage implements OnInit, AfterContentChecked, OnDestroy {
   @ViewChild('swiper') swiper: SwiperComponent
 config: SwiperOptions = {slidesPerView: 'auto' , effect: 'coverflow'}
   constructor(
+    private actionSheetController: ActionSheetController,
     private dataStorageService: DatastorageService,
     private modalController: ModalController,
     private userService: UserService,
@@ -51,15 +44,48 @@ config: SwiperOptions = {slidesPerView: 'auto' , effect: 'coverflow'}
   ) {
     this.username = this.userService.getUsername();
     this.loggedIn = this.userService.loggedIn;
-
   }
   async ngOnInit() {
     await this.synchroniseUser()
     if (this.swiper) {
       this.swiper.updateSwiper({})
     }
-
   }
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Options',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Logout',
+        role: 'destructive',
+        icon: 'trash',
+        id: 'delete-button',
+        data: {
+          type: 'delete'
+        },
+        handler: () => {
+          this.logout();
+        }
+      }, {
+        text: 'Profile',
+        icon: 'share',
+        handler: () => {
+          this.router.navigate(['/profile']);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role, data } = await actionSheet.onDidDismiss();
+    console.log('', role, data);
+  }
+
   onSelectCourse(course) {
     //when pressing course button, updates course's state to show that as the current course
     this.courseService.setCourse(course);
@@ -73,7 +99,6 @@ config: SwiperOptions = {slidesPerView: 'auto' , effect: 'coverflow'}
     //sets component variables to match the application's state
     var availableCourseNames = await this.userService.getCourses();
     for (let course of this.courseService.getAllCourses()){
-      console.log(availableCourseNames, course.title)
       for (let testCourse of availableCourseNames){
         if (testCourse.courseType == course.title){
           this.availableCourses.push(course)
@@ -83,14 +108,13 @@ config: SwiperOptions = {slidesPerView: 'auto' , effect: 'coverflow'}
     }
     this.username = this.userService.getUsername();
     this.userRole = this.userService.getUserRole();
-    if (this.userService.getUser().courses.length > 0){
-
+    if (this.userService.getUser().courses.length >= 1){
     this.userCourses = this.userService.getUser().courses
     this.userCourses= this.userCourses.sort((a, b) => {
       return a.courseDate - b.courseDate}).reverse()
-      this.courseDate = this.userService.getUser().courses[0].courseDate
-      this.courseType = this.userService.getUser().courses[0].courseType
-      this.studentList = this.userService.getUser().courses[0].students
+      this.courseDate = this.userCourses[0].courseDate
+      this.courseType = this.userCourses[0].courseType
+      this.studentList = this.userCourses[0].students
     }
 
   }
@@ -116,10 +140,11 @@ config: SwiperOptions = {slidesPerView: 'auto' , effect: 'coverflow'}
             this.userCourses = this.userCourses.sort((a, b) => {
               return a.courseDate - b.courseDate})
         
-              console.log(this.userCourses[0])
+              if (this.userCourses.length > 0){
               this.courseDate = this.userCourses[0].courseDate
               this.courseType = this.userCourses[0].courseType
               this.studentList = this.userCourses[0].students
+              }
             })
           }
         }
@@ -129,6 +154,5 @@ config: SwiperOptions = {slidesPerView: 'auto' , effect: 'coverflow'}
   }
 
   ngOnDestroy(){
-    console.log('destroy instan')
   }
 }
--- src/app/users/login/login.page.ts
+++ src/app/users/login/login.page.ts
@@ -17,17 +17,18 @@ export class LoginPage implements OnInit {
     private userService: UserService,
     private router: Router,
     private dataStorage: DatastorageService
-  ) {
-  }
-  clearMem(){
-    this.dataStorage.clear()
-    this.ngOnInit()
+  ) {}
+  /* Clearing the memory. */
+  clearMem() {
+    this.dataStorage.clear();
+    this.ngOnInit();
   }
 
+  /* Authenticating the user login, returning a message if it fails, and navigating the router to
+  the landing page if it is successful. */
   async login() {
-    //authenticates user login, returns message if fails, navigates router to landing page if successful
-    await this.userService.login(this.username, this.password)
-      if (!this.userService.loggedIn) {
+    await this.userService.login(this.username, this.password);
+    if (!this.userService.loggedIn) {
       this.incorrectCount++;
       this.incorrectPassword = `Incorrect details, please try again
       (${this.incorrectCount} attempts)`;
@@ -36,28 +37,30 @@ export class LoginPage implements OnInit {
     this.incorrectCount = 0;
     this.incorrectPassword = '';
     if (this.savelogin) {
-    await this.dataStorage.save('saveduserdetails', {
+      await this.dataStorage.save('saveduserdetails', {
         username: this.username,
         password: this.password,
       });
-    } else {await this.dataStorage.save('saveduserdetails', {username: "", password: ""})}
+    } else {
+      await this.dataStorage.save('saveduserdetails', {
+        username: '',
+        password: '',
+      });
+    }
     this.username = '';
     this.password = '';
-    this.savelogin = false
+    this.savelogin = false;
     this.router.navigate(['/landing'], { replaceUrl: true });
-
   }
 
-
- async ngOnInit() {
-    var details = await this.dataStorage.lookup('saveduserdetails')
-      if (details && details != {username: "", password: ""}) {
-        this.username = details.username;
-        this.password = details.password;
-          if (this.username.length > 0 && this.password.length > 0){
-            this.savelogin = true
-          }
-      };
+  async ngOnInit() {
+    var details = await this.dataStorage.lookup('saveduserdetails');
+    if (details && details != { username: '', password: '' }) {
+      this.username = details.username;
+      this.password = details.password;
+      if (this.username.length > 0 && this.password.length > 0) {
+        this.savelogin = true;
+      }
+    }
   }
-
 }
